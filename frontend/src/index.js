eb71f0eabc1ed36ed7f68c6ff8e0bfd7aaca0c2c
Laysa <laysa.viana@outlook.com>
2025-12-12 01:11:53 -0300

    refactor: refactor do index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ThemeModeProvider } from './context/ThemeContext';
-import { CssBaseline, GlobalStyles } from '@mui/material';
+import { ThemeProvider } from './context/ThemeContext';
+import { Layout } from './components/Layout/Layout';
+import { HomePage } from './pages/home/Home.page';
+import './styles/index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeModeProvider>
-      <CssBaseline />
-      <GlobalStyles
-        styles={(theme) => ({
-          body: {
-            margin: 0,
-            padding: 0,
-            backgroundColor: theme.palette.background.default,
-            color: theme.palette.text.primary,
-            transition: 'background-color 0.3s ease, color 0.3s ease',
-          },
-        })}
-      />
-      <App />
-    </ThemeModeProvider>
+    <ThemeProvider>
+      <Layout>
+        <HomePage />
+      </Layout>
+    </ThemeProvider>
   </React.StrictMode>
 );
