6f7f2145f6e5b3d336145d6685ff50d408b69211
Laysa <laysa.viana@outlook.com>
2025-12-12 20:00:35 -0300

    feat: add injectMobileFixes in raiz

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import injectMobileFixes from './styles/inject-mobile-fixes';
 import { ThemeProvider } from './context/ThemeContext';
 import { Layout } from './components/Layout/Layout';
 import { HomePage } from './pages/home/Home.page';
 import './styles/index.css';
+
+injectMobileFixes();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
